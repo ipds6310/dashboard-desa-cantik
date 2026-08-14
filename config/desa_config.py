9a2eb85cfc8f8ceb7cb5f1509c7b578feb33df52
Vivi Adelia <vivi.adelia@bps.go.id>
2026-08-14 10:57:13 +0800

    Update link spreadsheet data per desa

--- config/desa_config.py
+++ config/desa_config.py
@@ -47,24 +47,24 @@ DESA_CONFIG = {
         "foto": "kelgunting.jpeg",
 
        # DATA UMUM
-        "url_data": "https://docs.google.com/spreadsheets/d/1n6RkwCmYWQm4Uc21GtgZ0Tc6BracOxevkqgtEq8lMoY/edit?usp=sharing",
+        "url_data": "https://docs.google.com/spreadsheets/d/17WYCmQwopO0Zxjmnl9K4jwssZWVNuqtWQuOLBlglwDc/edit?usp=sharing",
 
         # KEPENDUDUKAN
-        "url_penduduk": "https://docs.google.com/spreadsheets/d/1Lq_4bictqMTRzKmFAQ5K4i6Tb_W9nTC07bbz-tM_HTU/edit?usp=sharing",
-        "url_penduduk_2023": "https://docs.google.com/spreadsheets/d/11nLD7leY49oRAgFKeTOyQHF0tD348vk64NNKCVkN4uk/edit?usp=sharing",
-        "url_pekerjaan": "https://docs.google.com/spreadsheets/d/1RPmfvPL4Bnr6on_rXtndeyiLazqeKqHRZctYKrgRq-s/edit?usp=sharing",
-        "url_pendidikan": "https://docs.google.com/spreadsheets/d/143uGikYZQwvTfgP4_GRjZOxey5_3SIeWp1Raw5ecrT8/edit?usp=sharing",
-        "url_etnis": "https://docs.google.com/spreadsheets/d/14Z8wDSU1h8F7xS20gMHekrHCQ2BjtVg4ql2RyLVekzg/edit?usp=sharing",
-        "url_agama": "https://docs.google.com/spreadsheets/d/17OruE9Jd9Qr76Tkc-uZioV7IZSi_wqmCllwbHWhEQsc/edit?usp=sharing",
+        "url_penduduk": "https://docs.google.com/spreadsheets/d/1OT2IOvR9aKd35Dyun-BpzRUF8M8cc0e1DCCuIcdKzmw/edit?usp=sharing",
+        "url_penduduk_2023": "https://docs.google.com/spreadsheets/d/12kajdotB46IdPrrSgj7k2qqvl4wvjEnYbfgbxvo8-h0/edit?usp=sharing",
+        "url_pekerjaan": "https://docs.google.com/spreadsheets/d/1R03QvPZGLaaqBeJbTbnodOaq7yV_ud4XuFsumevAaBI/edit?usp=sharing",
+        "url_pendidikan": "https://docs.google.com/spreadsheets/d/10JZy2X5or9xcEv6KD6i4IImQPhrpiV08slWpg_NOwcc/edit?usp=sharing",
+        "url_etnis": "https://docs.google.com/spreadsheets/d/1AZJtj7bgyS38iomFikAOFhuvoXVcMNq9M5N0mUrsqrk/edit?usp=sharing",
+        "url_agama": "https://docs.google.com/spreadsheets/d/1PkhbGp9DQLYgS0ZPl8h9WT3QueCz8lUBJlPPB3dIadM/edit?usp=sharing",
 
         # FASILITAS
-        "url_fasilitas": "https://docs.google.com/spreadsheets/d/1KFo2hs7pTIqJ555BCx-tpn2foaFpV3i7K9PcKVbFR7A/edit?usp=sharing",
+        "url_fasilitas": "https://docs.google.com/spreadsheets/d/1-39Is_u72RFhnXXVAGUpxAXpmvTdDVgfw6gJ1950yR8/edit?usp=sharing",
 
         # PERTANIAN
-        "url_pertanian": "https://docs.google.com/spreadsheets/d/1n4OhTNKWiVf6zRYJgzcHU3hXcwTG0CCmEBQFML49ENc/edit?usp=sharing",
+        "url_pertanian": "https://docs.google.com/spreadsheets/d/1tWLBMhEKoSjoTNM2jb24JI2VrRlC6-uerVBq3iIZAnk/edit?usp=sharing",
 
          #Unduh
-        "url_buku" : "https://docs.google.com/spreadsheets/d/1iVqggywv8FP8yYlW844D4y5IHQMJagPtdjQCfUucP9M/edit?usp=sharing",
+        "url_buku" : "https://docs.google.com/spreadsheets/d/135PFIEgaaDTwItfFOhx-mtTMhlMeOu_xFmek118wPBk/edit?usp=sharing",
 
         "email": "sarigadung865@gmail.com",
         "alamat": "Jalan Pegangsaan RT/RW. 001/001, Desa Sarigadung, Kecamatan Simpang Empat, Kabupaten Tanah Bumbu",
@@ -83,24 +83,24 @@ DESA_CONFIG = {
         "label_wilayah": "Desa",
 
         # DATA UMUM
-        "url_data": "https://docs.google.com/spreadsheets/d/1n6RkwCmYWQm4Uc21GtgZ0Tc6BracOxevkqgtEq8lMoY/edit?usp=sharing",
+        "url_data": "https://docs.google.com/spreadsheets/d/1Guk7PUPY5fXAUPB7sWK9FG7dwBLwPEmbGFvRDxcWpGM/edit?usp=sharing",
 
         # KEPENDUDUKAN
-        "url_penduduk": "https://docs.google.com/spreadsheets/d/1Lq_4bictqMTRzKmFAQ5K4i6Tb_W9nTC07bbz-tM_HTU/edit?usp=sharing",
-        "url_penduduk_2023": "https://docs.google.com/spreadsheets/d/11nLD7leY49oRAgFKeTOyQHF0tD348vk64NNKCVkN4uk/edit?usp=sharing",
-        "url_pekerjaan": "https://docs.google.com/spreadsheets/d/1RPmfvPL4Bnr6on_rXtndeyiLazqeKqHRZctYKrgRq-s/edit?usp=sharing",
-        "url_pendidikan": "https://docs.google.com/spreadsheets/d/143uGikYZQwvTfgP4_GRjZOxey5_3SIeWp1Raw5ecrT8/edit?usp=sharing",
-        "url_etnis": "https://docs.google.com/spreadsheets/d/14Z8wDSU1h8F7xS20gMHekrHCQ2BjtVg4ql2RyLVekzg/edit?usp=sharing",
-        "url_agama": "https://docs.google.com/spreadsheets/d/17OruE9Jd9Qr76Tkc-uZioV7IZSi_wqmCllwbHWhEQsc/edit?usp=sharing",
+        "url_penduduk": "https://docs.google.com/spreadsheets/d/191VOMoslwMqA4xzVPa5PRWbvBf0sguUw_Zi7sHgulnQ/edit?usp=sharing",
+        "url_penduduk_2023": "https://docs.google.com/spreadsheets/d/1uwDdTcjfYg4AgjKr26X8wl3PDy5nOR01IsLgENmoDYc/edit?usp=sharing",
+        "url_pekerjaan": "https://docs.google.com/spreadsheets/d/1MaBefeyqaFNvai23GbkD4Ozk_ctrld5Gn5nQTHm2NGc/edit?usp=sharing",
+        "url_pendidikan": "https://docs.google.com/spreadsheets/d/13gj3V6IorRVZI5VPON1APTH2FNAFAMihcnb-6uddh2s/edit?usp=sharing",
+        "url_etnis": "https://docs.google.com/spreadsheets/d/1u9iNx9nERCq3O98_peZgkZDbfjRm-onW1ZuFCsVTyhc/edit?usp=sharing",
+        "url_agama": "https://docs.google.com/spreadsheets/d/12lLHYT_pM2xZj6dRj_TO6N8CWnhiKRiS2z7WcHVPNWk/edit?usp=sharing",
 
         # FASILITAS
-        "url_fasilitas": "https://docs.google.com/spreadsheets/d/1KFo2hs7pTIqJ555BCx-tpn2foaFpV3i7K9PcKVbFR7A/edit?usp=sharing",
+        "url_fasilitas": "https://docs.google.com/spreadsheets/d/1t9n4ouYS5J3YvhioZf_VBTXmHJVlvzj-vTs9OxIX69w/edit?usp=sharing",
 
         # PERTANIAN
-        "url_pertanian": "https://docs.google.com/spreadsheets/d/1n4OhTNKWiVf6zRYJgzcHU3hXcwTG0CCmEBQFML49ENc/edit?usp=sharing",
+        "url_pertanian": "https://docs.google.com/spreadsheets/d/1yDsg20pAfFT_tY-PWk1qcy1dVxf8w8cnUhznVhHfVHw/edit?usp=sharing",
 
         #Unduh
-        "url_buku": "https://docs.google.com/spreadsheets/d/1iVqggywv8FP8yYlW844D4y5IHQMJagPtdjQCfUucP9M/edit?usp=sharing",
+        "url_buku": "https://docs.google.com/spreadsheets/d/1v3Z2RRv0QDPSn0DSAq_2ylOYnFOCjK2VA0yy6Z4kduI/edit?usp=sharing",
 
         "email": "-",
         "alamat": "Jln A Yani RT 03 RW 01, Sungai Dua, Kec. Simpang Empat, Kabupaten Tanah Bumbu",
